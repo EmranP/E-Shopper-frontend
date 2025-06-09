f77353577d65fd9e16aec5d6bf844370a84186b7
Emran <nightsking12345@gmail.com>
2025-06-10 00:31:48 +0500

    Init config && add .env.example

--- src/shared/config/axiosInstance.ts
+++ src/shared/config/axiosInstance.ts
@@ -10,7 +10,7 @@ export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL
 
 const $api = axios.create({
 	withCredentials: true,
-	baseURL: BASE_API_URL,
+	baseURL: `${BASE_API_URL}/api`,
 	headers: {
 		'Content-Type': 'application/json',
 	},
